ed6d3136ed28f93bb184aff8313e6c2916b90550
tocka <tocka.ayman@virgingates.com>
2018-08-09 14:06:57 +0200

    Update API

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,10 @@ COPY --chown=gradle:gradle . /home/gradle/src
 WORKDIR /home/gradle/src
 RUN gradle build
 
-#FROM openjdk:8-jre-alpine
-#EXPOSE 8080
-#COPY --from=builder /home/gradle/src/employees.check.in/build/libs/employees-check-in-1.0-SNAPSHOT.jar /app/
-#COPY --from=builder /home/gradle/src/employees.check.in/tokens/ /tokens
-#
-#WORKDIR /app
-#CMD ["java", "-jar", "/app/employees-check-in-1.0-SNAPSHOT.jar"]
+FROM openjdk:8-jre-alpine
+EXPOSE 8080
+COPY --from=builder /home/gradle/src/employees-check-in/build/libs/employees-check-in-1.0-SNAPSHOT.jar /app/
+COPY --from=builder /home/gradle/src/employees-check-in/tokens/ /tokens
+
+WORKDIR /app
+CMD ["java", "-jar", "/app/employees-check-in-1.0-SNAPSHOT.jar"]
---
+++ src/main/kotlin/com/virgingates/test.java
@@ -0,0 +1,35 @@
+package com.virgingates;
+
+public class test {
+
+    public static void main(String[] args) {
+        test t = new test();
+        t.test1();
+        t.test2();
+    }
+
+    public void test1() {
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello");
+        }
+    }
+
+    public void test2() {
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello");
+        }
+    }
+
+}
